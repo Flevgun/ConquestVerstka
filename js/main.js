be18595c9292614163dcf6f04bbff0c67b7e29c4
Flevgun <sansan0851.sk@gmail.com>
2023-02-28 18:30:46 +0500

    html + sass

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//slider
+
+let slideIndex = 1
+    slides = document.querySelectorAll('.slider-item')
+    prev = document.querySelector('.prev')
+    next = document.querySelector('.next')
+    numbersWrap = document.querySelector('.slider-numbers')
+    number = document.querySelectorAll('.number')
+
